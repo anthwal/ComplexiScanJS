80e96d5d0f679e67077cb9c6bc4377a3cfc167cd
Anmol71 <anmolsharma1923@gmail.com>
2025-02-27 11:24:55 +0530

    add: analyzer to calculate basic cognitive complexity without logical operators

---
+++ src/analyzer.ts
@@ -0,0 +1,102 @@
+// src/analyzer.ts
+import { readFileSync } from "fs";
+import { Tokenizer, TokenType, Token } from "./tokenizer";
+
+interface FunctionAnalysis {
+  name: string;
+  complexity: number;
+  threshold: number;
+}
+
+export function analyze(
+  filePath: string,
+  threshold: number
+): FunctionAnalysis[] {
+  const source = readFileSync(filePath, "utf8");
+  const tokenizer = new Tokenizer(source);
+  const tokens = tokenizer.tokenize();
+  return analyzeFunctions(tokens, threshold);
+}
+
+function analyzeFunctions(
+  tokens: Token[],
+  threshold: number
+): FunctionAnalysis[] {
+  const functions: FunctionAnalysis[] = [];
+  let currentFunction: FunctionAnalysis | null = null;
+  let braceStack = 0;
+  let currentNestingLevel = 0;
+  const nestingStack: number[] = [];
+
+  for (let i = 0; i < tokens.length; i++) {
+    const token = tokens[i];
+
+    if (token.type === TokenType.Function || token.type === TokenType.Arrow) {
+      const name = getFunctionName(tokens, i);
+      currentFunction = { name, complexity: 0, threshold }; // Initialize to 0
+      functions.push(currentFunction);
+    }
+
+    if (token.type === TokenType.LeftBrace) {
+      braceStack++;
+    } else if (token.type === TokenType.RightBrace) {
+      braceStack--;
+      if (braceStack === 0) {
+        currentFunction = null;
+        currentNestingLevel = 0;
+        nestingStack.length = 0;
+      } else if (nestingStack.length > 0) {
+        currentNestingLevel = nestingStack.pop()!;
+      }
+    }
+
+    if (currentFunction) {
+      const complexityIncrement = calculateTokenComplexity(token);
+      if (complexityIncrement > 0) {
+        currentFunction.complexity += complexityIncrement + currentNestingLevel;
+        if (isNestingToken(token)) {
+          nestingStack.push(currentNestingLevel);
+          currentNestingLevel += 1;
+        }
+      }
+    }
+  }
+
+  return functions;
+}
+
+function getFunctionName(tokens: Token[], index: number): string {
+  for (let i = index + 1; i < tokens.length; i++) {
+    if (tokens[i].type === TokenType.Identifier) {
+      return tokens[i].value;
+    }
+  }
+  return "<anonymous>";
+}
+
+function calculateTokenComplexity(token: Token): number {
+  switch (token.type) {
+    case TokenType.If:
+    case TokenType.While:
+    case TokenType.For:
+    case TokenType.Do:
+    case TokenType.Case:
+    case TokenType.Catch:
+    case TokenType.LogicalAnd:
+    case TokenType.LogicalOr:
+      return 1;
+    default:
+      return 0;
+  }
+}
+
+function isNestingToken(token: Token): boolean {
+  return [
+    TokenType.If,
+    TokenType.While,
+    TokenType.For,
+    TokenType.Do,
+    TokenType.Case,
+  ].includes(token.type);
+}
+
--- test.js
+++ test.js
@@ -1,4 +1,168 @@
-function myFunc() {
-  console.log("hello", i);
+function test(name) {
+  for (let i = 0; i <= 5; i++) {
+    if (i == 5) {
+      name = "Anmol";
+      console.log("Value is 5", name);
+      if (i == 4) {
+        name = "Anmol";
+        console.log("Value is 5", name);
+      }
+    }
+  }
+  for (let i = 0; i <= 5; i++) {
+    if (i == 5) {
+      name = "Anmol";
+      console.log("Value is 5", name);
+      if (i == 4) {
+        name = "Anmol";
+        console.log("Value is 5", name);
+      }
+    }
+  }
+  for (let i = 0; i <= 5; i++) {
+    if (i == 5) {
+      name = "Anmol";
+      console.log("Value is 5", name);
+      if (i == 4) {
+        name = "Anmol";
+        console.log("Value is 5", name);
+      }
+    }
+  }
 }
-myFunc();
+function test1(name) {
+  for (let i = 0; i <= 5; i++) {
+    if (i == 5) {
+      name = "Anmol";
+      console.log("Value is 5", name);
+      if (i == 4) {
+        name = "Anmol";
+        console.log("Value is 5", name);
+      }
+    }
+  }
+  for (let i = 0; i <= 5; i++) {
+    if (i == 5) {
+      name = "Anmol";
+      console.log("Value is 5", name);
+      if (i == 4) {
+        name = "Anmol";
+        console.log("Value is 5", name);
+      }
+    }
+  }
+  for (let i = 0; i <= 5; i++) {
+    if (i == 5) {
+      name = "Anmol";
+      console.log("Value is 5", name);
+      if (i == 4) {
+        name = "Anmol";
+        console.log("Value is 5", name);
+      }
+    }
+  }
+}
+function test2(name) {
+  for (let i = 0; i <= 5; i++) {
+    if (i == 5) {
+      name = "Anmol";
+      console.log("Value is 5", name);
+      if (i == 4) {
+        name = "Anmol";
+        console.log("Value is 5", name);
+      }
+    }
+  }
+  for (let i = 0; i <= 5; i++) {
+    if (i == 5) {
+      name = "Anmol";
+      console.log("Value is 5", name);
+      if (i == 4) {
+        name = "Anmol";
+        console.log("Value is 5", name);
+      }
+    }
+  }
+  for (let i = 0; i <= 5; i++) {
+    if (i == 5) {
+      name = "Anmol";
+      console.log("Value is 5", name);
+      if (i == 4) {
+        name = "Anmol";
+        console.log("Value is 5", name);
+      }
+    }
+  }
+}
+function test3(name) {
+  for (let i = 0; i <= 5; i++) {
+    if (i == 5) {
+      name = "Anmol";
+      console.log("Value is 5", name);
+      if (i == 4) {
+        name = "Anmol";
+        console.log("Value is 5", name);
+      }
+    }
+  }
+  for (let i = 0; i <= 5; i++) {
+    if (i == 5) {
+      name = "Anmol";
+      console.log("Value is 5", name);
+      if (i == 4) {
+        name = "Anmol";
+        console.log("Value is 5", name);
+      }
+    }
+  }
+  // for (let i = 0; i <= 5; i++) {
+  //   if (i == 5) {
+  //     name = "Anmol";
+  //     console.log("Value is 5", name);
+  //     if (i == 4) {
+  //       name = "Anmol";
+  //       console.log("Value is 5", name);
+  //     }
+  //   }
+  // }
+}
+function showLogin(name) {
+  for (let i = 0; i <= 5; i++) {
+    if (i == 5) {
+      name = "Anmol";
+      console.log("Value is 5", name);
+      if (i == 4) {
+        name = "Anmol";
+        console.log("Value is 5", name);
+      }
+    }
+    if (i == 5) {
+      name = "Anmol";
+      console.log("Value is 5", name);
+      if (i == 4) {
+        name = "Anmol";
+        console.log("Value is 5", name);
+      }
+    }
+  }
+  for (let i = 0; i <= 5; i++) {
+    if (i == 5) {
+      name = "Anmol";
+      console.log("Value is 5", name);
+      if (i == 4) {
+        name = "Anmol";
+        console.log("Value is 5", name);
+      }
+    }
+  }
+  for (let i = 0; i <= 5; i++) {
+    if (i == 5) {
+      name = "Anmol";
+      console.log("Value is 5", name);
+      if (i == 4) {
+        name = "Anmol";
+        console.log("Value is 5", name);
+      }
+    }
+  }
+}
