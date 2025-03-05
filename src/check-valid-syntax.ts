c21af50ad4b5574102e619956615476bfedd51cd
Anmol71 <anmolsharma1923@gmail.com>
2025-03-05 09:54:24 +0530

    update: syntax validator

--- src/check-valid-syntax.ts
+++ src/check-valid-syntax.ts
@@ -1,8 +1,6 @@
-import * as vm from "vm";
-
 export function isValidSyntax(source: string, filePath: string): boolean {
   try {
-    new vm.Script(source, { filename: filePath });
+    new Function(source);
     return true;
   } catch (err: unknown) {
     console.error(`❌ Syntax Error in '${filePath}':\n${err}`);
